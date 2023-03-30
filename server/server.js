6a256c6f4729d4ad7fe02abe97de05e8bcd98002
amritrai5757 <amritrai5757@gmail.com>
2023-03-30 21:23:47 +0530

    frontend for login

--- server/server.js
+++ server/server.js
@@ -1,60 +1,32 @@
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-const passport = require('passport');
-const session = require('express-session');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
-// Initialize Express app
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const passport = require("passport");
+const authRoute = require("./routes/auth");
+const cookieSession = require("cookie-session");
+const passportStrategy = require("./passport");
 const app = express();
 
-// Serve static files from React app
-app.use(express.static(path.join('C:', 'Users', 'amrit', 'OneDrive', 'Desktop', 'Solving-for-India', 'client', 'build')));
+app.use(
+	cookieSession({
+		name: "session",
+		keys: ["cyberwolve"],
+		maxAge: 24 * 60 * 60 * 100,
+	})
+);
 
-// Connect to MongoDB
-mongoose.connect('mongodb+srv://solvingforindiagfg:TEBEiwOKC15SX4v7@cluster0.e8ey2u2.mongodb.net/test?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log('Connected to MongoDB');
-}).catch(err => {
-  console.log(err);
-});
-
-// Configure Passport.js for Google sign-in
-passport.use(new GoogleStrategy({
-  clientID: '876687008029-racad9ri4fpoofmvhphnj4c32c27q0pr.apps.googleusercontent.com',
-  clientSecret: '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',
-  callbackURL: '/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
-  // Add code here to handle user authentication
-}));
-
-// Configure session middleware
-app.use(session({
-  secret: 'mysecretkey',
-  resave: false,
-  saveUninitialized: false
-}));
-
-// Initialize Passport.js middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
-// Define authentication routes
-app.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
-app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }), (req, res) => {
-  // Successful authentication, redirect to home page
-  res.redirect('/');
-});
+app.use(
+	cors({
+		origin: "http://localhost:3000",
+		methods: "GET,POST,PUT,DELETE",
+		credentials: true,
+	})
+);
 
-// Serve React app
-app.get('*', (req, res) => {
-  res.sendFile(path.join('C:', 'Users', 'amrit', 'OneDrive', 'Desktop', 'Solving-for-India', 'client', 'build', 'index.html'));
-});
+app.use("/auth", authRoute);
 
-// Start server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listenting on port ${port}...`));
