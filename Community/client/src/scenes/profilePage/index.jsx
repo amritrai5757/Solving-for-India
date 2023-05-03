73b7113d74be3c5642035c7b1a4b6d5fa5550ac8
amritrai5757 <amritrai5757@gmail.com>
2023-05-04 01:30:17 +0530

    updated url

--- Community/client/src/scenes/profilePage/index.jsx
+++ Community/client/src/scenes/profilePage/index.jsx
@@ -15,7 +15,7 @@ const ProfilePage = () => {
   const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
 
   const getUser = async () => {
-    const response = await fetch(`https://khetify-community.vercel.app/users/${userId}`, {
+    const response = await fetch(`https://communityserver.onrender.com/users/${userId}`, {
       method: "GET",
       headers: { Authorization: `Bearer ${token}` },
     });
